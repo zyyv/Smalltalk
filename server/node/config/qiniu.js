80af25a2a184363c2a484358e4304063591d1bda
Chris_zy <1633711653@qq.com>
2020-12-11 16:06:05 +0800

    update qiniu config

--- server/node/config/qiniu.js
+++ server/node/config/qiniu.js
@@ -1,6 +1,6 @@
 module.exports = {
   Ak: 'rbLH01OfXV9dUw4ca25lkwhBVEP3u6Tw-J2RItKE',
   Sk: '0BjBGIgVhUuDflnqYIO_R8n_L8no_BFgZCALpZVq',
-  Bucket: 'smalltalk', // 空间名称
-  BucketDomain: 'http://qjm1z1hlr.hn-bkt.clouddn.com/' // 空间域名
+  Bucket: 'chrisxixihaha', // 空间名称
+  BucketDomain: 'http://ql61yf5hl.hn-bkt.clouddn.com/' // 空间域名
 }
