b69c170e14cfdd9fb61dd9cc82112b3ba0774df6
chriszhu <1633711653@qq.com>
2020-12-28 14:56:39 +0800

    update

--- server/node/routes/index.js
+++ server/node/routes/index.js
@@ -5,9 +5,9 @@ const { key } = require('../utils/jwt')
 const unlessRoutes = require('../config/unless')
 const { success } = require('../middleware/result')
 const api = require('./api')
-const client = require('./client')
+// const client = require('./client')
 
-router.use('/', client.routes(), client.allowedMethods())
+// router.use('/', client.routes(), client.allowedMethods())
 router.use(koaJwt({ secret: key }).unless({
   path: unlessRoutes
 }))
